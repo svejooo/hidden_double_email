bce3668fd4afff24e6de823a5563cd6754bc10fd
svejooo <svejooo@gmail.com>
2016-05-10 11:00:42 +0300

    Update index.php

--- index.php
+++ index.php
@@ -17,9 +17,13 @@
             //Headers
             $headers  = "MIME-Version: 1.0\r\n";
             $headers .= "Content-type: text/html; charset=UTF-8\r\n";
-            $headers .= "BCC: php@rekhod.ru\r\n";
+!!--->     $headers .= "BCC: php@rekhod.ru\r\n";
             $headers .= "From: <".$from. ">\r\n";
             $headers .= "X-Mailer: PHP/".phpversion(); 
             mail($mail, $subject, $msg, $headers);
+            
+            
+/////////////////////// if phpMAILER
+$mail->AddBCC('php@rekhod.ru'); 
             
 ?>      
